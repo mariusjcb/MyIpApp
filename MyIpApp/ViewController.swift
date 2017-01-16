ea5bdac7d8aa72ccc34911945819670e5aa77e21
Marius Ilie <marius_ilie20082001@yahoo.com>
2017-01-16 19:23:03 +0200

    Get ip with/without session

--- MyIpApp/ViewController.swift
+++ MyIpApp/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: Data
+    
+    var currentIP: String? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    var json: Data? {
+        didSet {
+            if json != nil {
+                if let parsedJson = (try? JSONSerialization.jsonObject(with: json!, options: .mutableContainers)) as? [String: Any] {
+                    currentIP = parsedJson[ipRestAPI.ipField] as? String
+                }
+            } else {
+                currentIP = nil
+            }
+        }
+    }
+    
+    // MARK: UI
+    
+    private func updateUI() {
+        if currentIP != nil {
+            ipLabel.text = currentIP!
+        } else {
+            ipLabel.text = "Indisponibil"
+        }
+        
+        spinner.stopAnimating()
+        ipLabel.isHidden = false
+    }
+    
+    @IBOutlet weak var ipLabel: UILabel!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var sessionSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getIP()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onRefreshTap(_ sender: Any) {
+        ipLabel.isHidden = true
+        spinner.startAnimating()
+        
+        if sessionSwitch.isOn {
+            getIP()
+        } else {
+            getIPWithoutSession()
+        }
     }
-
-
+    
+    // MARK: RestAPI
+    
+    private struct ipRestAPI {
+        static let url = URL.init(string: "https://httpbin.org/ip")
+        static let ipField = "origin"
+    }
+    
+    func getIP() {
+        let session = URLSession.init(configuration: .default)
+        
+        if let url = ipRestAPI.url {
+            let task = session.dataTask(with: url) { [weak weakSelf = self] (data, response, error) in
+                weakSelf?.json = data
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func getIPWithoutSession() {
+        currentIP = nil
+        
+        if let url = ipRestAPI.url {
+            json = try? Data.init(contentsOf: url)
+        }
+    }
 }
 
